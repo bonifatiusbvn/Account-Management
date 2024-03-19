19987534a028bc500d4dc3430f17e2829e2b94f3
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2024-03-19 12:33:58 +0530

    Janvi - Add Company

---
+++ AccountManegment.Repo/Interface/Repository/Company/ICompany.cs
@@ -0,0 +1,14 @@
+﻿using AccountManagement.DBContext.Models.ViewModels.CompanyModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManagement.Repository.Interface.Repository.Company
+{
+    public interface ICompany
+    {
+        Task<CompanyModel> CreateCompany();
+    }
+}
