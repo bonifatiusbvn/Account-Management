d14a18df989dfaf943482e59ee922a8819204525
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2024-03-20 18:24:53 +0530

    Rahi- Folder Add

---
+++ AccountManegments.Web/Controllers/SiteMasterController.cs
@@ -0,0 +1,24 @@
+﻿using AccountManegments.Web.Helper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountManegments.Web.Controllers
+{
+    public class SiteMasterController : Controller
+    {
+        public SiteMasterController(WebAPI webAPI, APIServices aPIServices, IWebHostEnvironment environment)
+        {
+            WebAPI = webAPI;
+            APIServices = aPIServices;
+            Environment = environment;
+        }
+
+        public WebAPI WebAPI { get; }
+        public APIServices APIServices { get; }
+        public IWebHostEnvironment Environment { get; }
+
+        public IActionResult SiteListView()
+        {
+            return View();
+        }
+    }
+}
