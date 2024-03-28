17f562dfd74b2f08bc715ec38e7c101720372187
Bhavin Fitter <bhavin.bonifatius@gmail.com>
2024-03-28 18:53:25 +0530

    Bhavin - Error Solved

--- AccountManegments.Web/wwwroot/Moduls/MasterListScript.js
+++ AccountManegments.Web/wwwroot/Moduls/MasterListScript.js
@@ -90,19 +90,3 @@ function GetUserRole() {
     });
 }
 
-$(document).on("click", ".plus", function () {
-    debugger
-    updateProductQuantity($(this).closest(".product"), 1);
-    return
-});
-
-$(document).on("click", ".minus", function () {
-    debugger
-    updateProductQuantity($(this).closest(".product"), -1);
-    return
-});
-$(document).on("click", "#remove", function () {
-    var row = $(this).closest("tr");
-    row.remove();
-    updateTotals();
-});
