2d49b5f92f0c19076d6d4141eb0009c1c07ea9d4
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2024-03-30 15:04:11 +0530

    Rahi-Create Supplier Invoice

--- AccountManegments.Web/wwwroot/Moduls/InvoiceMasterScript.js
+++ AccountManegments.Web/wwwroot/Moduls/InvoiceMasterScript.js
@@ -1,4 +1,5 @@
-﻿$(document).ready(function () {
+﻿AllSupplierInvoiceListTable()
+$(document).ready(function () {
     var itemCounter = 0;
 
     $("#addItemBtn").click(function () {
@@ -56,3 +57,110 @@
     });
 
 });
+
+function AllSupplierInvoiceListTable() {
+   
+    var searchText = $('#txtSupplierInvoiceSearch').val();
+    var searchBy = $('#SupplierInvoiceSearchBy').val();
+
+    $.get("/InvoiceMaster/SupplierInvoiceListAction", { searchBy: searchBy, searchText: searchText })
+        .done(function (result) {
+
+
+            $("#SupplierInvoicebody").html(result);
+        })
+        .fail(function (error) {
+            console.error(error);
+        });
+}
+
+function filterSupplierInvoiceTable() {
+
+    var searchText = $('#txtSupplierInvoiceSearch').val();
+    var searchBy = $('#SupplierInvoiceSearchBy').val();
+
+    $.ajax({
+        url: '/InvoiceMaster/SupplierInvoiceListAction',
+        type: 'GET',
+        data: {
+            searchText: searchText,
+            searchBy: searchBy
+        },
+        success: function (result) {
+            $("#SupplierInvoicebody").html(result);
+        },
+        error: function (xhr, status, error) {
+
+        }
+    });
+}
+
+function SupplierInvoicesortTable() {
+    var sortBy = $('#SortBySupplierInvoice').val();
+    $.ajax({
+        url: '/InvoiceMaster/SupplierInvoiceListAction',
+        type: 'GET',
+        data: {
+            sortBy: sortBy
+        },
+        success: function (result) {
+            $("#SupplierInvoicebody").html(result);
+        },
+        error: function (xhr, status, error) {
+
+        }
+    });
+}
+
+function DeleteSupplierInvoiceDetails(InvoiceId)
+{
+    debugger
+    Swal.fire({
+        title: "Are you sure want to Delete This?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, Delete it!",
+        cancelButtonText: "No, cancel!",
+        confirmButtonClass: "btn btn-primary w-xs me-2 mt-2",
+        cancelButtonClass: "btn btn-danger w-xs mt-2",
+        buttonsStyling: false,
+        showCloseButton: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: '/InvoiceMaster/DeleteSupplierInvoiceDetails?InvoiceId=' + InvoiceId,
+                type: 'POST',
+                dataType: 'json',
+                success: function (Result) {
+
+                    Swal.fire({
+                        title: Result.message,
+                        icon: 'success',
+                        confirmButtonColor: '#3085d6',
+                        confirmButtonText: 'OK'
+                    }).then(function () {
+                        window.location = '/InvoiceMaster/SupplierInvoiceListView';
+                    })
+                },
+                error: function () {
+                    Swal.fire({
+                        title: "Can't Delete Site!",
+                        icon: 'warning',
+                        confirmButtonColor: '#3085d6',
+                        confirmButtonText: 'OK',
+                    }).then(function () {
+                        window.location = '/InvoiceMaster/SupplierInvoiceListView';
+                    })
+                }
+            })
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+
+            Swal.fire(
+                'Cancelled',
+                'Site Have No Changes.!!😊',
+                'error'
+            );
+        }
+    });   
+}
