4630edccc5ca7a2dc36abfb09327c9cd0d93bb98
Ravi Dalal <ravi.dalal@alliancetek.com>
2024-04-03 11:13:39 +0530

    Formpermissin Model

---
+++ AccountManegment.DBContext/Models/ViewModels/UserModels/FromPermission.cs
@@ -0,0 +1,10 @@
+﻿namespace AccountManegments.Web.Models
+{
+    public class FromPermission
+    {
+        public string FormName { get; set; }
+        public bool View { get; set; }
+        public bool Edit { get; set; }
+        public bool Delete { get; set; }
+    }
+}
