17014ca8d5ca368d43cf9a169ed9be7f67067c58
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2024-04-04 15:23:33 +0530

    Rahi- Display Invoice View

--- AccountManegments.Web/Controllers/InvoiceMasterController.cs
+++ AccountManegments.Web/Controllers/InvoiceMasterController.cs
@@ -262,7 +262,12 @@ namespace AccountManegments.Web.Controllers
                 if (response.code == 200)
                 {
                     order = JsonConvert.DeserializeObject<SupplierInvoiceMasterView>(response.data.ToString());
-                    response.data = order;
+                    var number = order.TotalAmount;
+                    var totalAmountInWords = NumberToWords((int)number);
+                    ViewData["TotalAmountInWords"] = totalAmountInWords + " " + "Only";
+                    var gstamt = order.TotalGstamount;
+                    var totalGstInWords = NumberToWords((int)gstamt);
+                    ViewData["TotalGstInWords"] = totalGstInWords + " " + "Only";
                 }
                 return View(order);
             }
@@ -270,6 +275,54 @@ namespace AccountManegments.Web.Controllers
             {
                 throw ex;
             }
+        }
+
+        public static string NumberToWords(int number)
+        {
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + NumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
+            {
+                words += NumberToWords(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+            }
+            return words;
         }
     }
 }
