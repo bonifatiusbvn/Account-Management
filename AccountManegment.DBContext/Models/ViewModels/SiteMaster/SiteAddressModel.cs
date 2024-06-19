7b013f5c7ba0cfc2e4f6f1f964f0b7bbe571a14e
Bhavin Fitter <bhavin.bonifatius@gmail.com>
2024-06-19 12:35:09 +0530

    Bhavin - Model Add

---
+++ AccountManegment.DBContext/Models/ViewModels/SiteMaster/SiteAddressModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManagement.DBContext.Models.ViewModels.SiteMaster
+{
+    public class SiteAddressModel
+    {
+        public int Aid { get; set; }
+
+        public Guid SiteId { get; set; }
+
+        public string Address { get; set; } = null!;
+
+        public bool? IsDeleted { get; set; }
+    }
+}
