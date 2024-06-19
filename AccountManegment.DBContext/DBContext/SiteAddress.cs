7f295232509b617f6647bd8f535ba9411a847104
Bhavin Fitter <bhavin.bonifatius@gmail.com>
2024-06-19 12:10:19 +0530

    Bhavin - New Table Add

---
+++ AccountManegment.DBContext/DBContext/SiteAddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AccountManagement.API;
+
+public partial class SiteAddress
+{
+    public int Aid { get; set; }
+
+    public Guid SiteId { get; set; }
+
+    public string Address { get; set; } = null!;
+
+    public bool? IsDeleted { get; set; }
+
+    public virtual Site Site { get; set; } = null!;
+}
