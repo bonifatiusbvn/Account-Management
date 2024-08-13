c36f48820dc618b3d118fc897e2f0b55fb4ae0d2
bonifatiusbvn <bhavin.bonifatius@gmail.com>
2024-08-13 18:52:55 +0530

    Pranali-Add File

---
+++ AccountManegment.DBContext/Models/ViewModels/SiteMaster/GroupMasterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManagement.DBContext.Models.ViewModels.SiteMaster
+{
+    public class GroupMasterModel
+    {
+        public int Id { get; set; }
+
+        public string GroupName { get; set; } = null!;
+
+        public List<SiteNameList>? SiteList { get; set; }
+    }
+    public class SiteNameList
+    {
+        public Guid SiteId { get; set; }
+    }
+}
---
+++ AccountManegment.DBContext/Models/ViewModels/SiteMaster/SiteDeliveryAddressModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManagement.DBContext.Models.ViewModels.SiteMaster
+{
+    public class SiteDeliveryAddressModel
+    {
+        public int Id { get; set; }
+
+        public Guid SiteId { get; set; }
+
+        public string Address { get; set; } = null!;
+
+        public bool? IsDeleted { get; set; }
+    }
+}
