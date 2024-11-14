fcdd1fcd3df963f7f4fa437267a6f8ee045dad8a
Bhavin Fitter <bhavin.bonifatius@gmail.com>
2024-11-14 14:52:57 +0530

    Bhavin - File Add

---
+++ AccountManegment.DBContext/Models/ViewModels/SiteMaster/UserSiteLIstModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManagement.DBContext.Models.ViewModels.SiteMaster
+{
+    public class UserSiteListModel
+    {
+        public Guid SiteId { get; set; }
+        public string SiteName { get; set; } = null!;
+    }
+}
