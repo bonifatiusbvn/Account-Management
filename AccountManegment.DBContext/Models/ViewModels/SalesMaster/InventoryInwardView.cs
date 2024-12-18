2125793400bc5d63042b26e71f77892c4c50e186
Bhavin Fitter <bhavin.bonifatius@gmail.com>
2024-12-18 17:51:49 +0530

    Bhavin - Error Solve

---
+++ AccountManegment.DBContext/Models/ViewModels/SalesMaster/InventoryInwardView.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManagement.DBContext.Models.ViewModels.SalesMaster
+{
+    public class InventoryInwardView
+    {
+        public Guid Id { get; set; }
+
+        public Guid? SiteId { get; set; }
+
+        public Guid ItemId { get; set; }
+
+        public string ItemName { get; set; } = null!;
+
+        public int UnitTypeId { get; set; }
+        public string? UnitName { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public string? Details { get; set; }
+
+        public bool? IsApproved { get; set; }
+
+        public bool? IsDeleted { get; set; }
+
+        public Guid CreatedBy { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public Guid? UpdatedBy { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
